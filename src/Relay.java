83e2449851a0bf4857fabe52a5fb99262f4b8011
Ramencozo <ramencozo.paradin@gmail.com>
2012-07-23 10:41:42 +0900

    Update src/Relay.java

--- src/Relay.java
+++ src/Relay.java
@@ -1,3 +1,10 @@
+// 継電器(リレー)クラス
+// コイルが一個、接点が一個の想定
+// 励磁状態または接点の接続/切断はbooleanで定義
+// solenoidState: コイルが励磁状態か否か(励磁状態ならtrue)
+// switchState: 接点が接続されているか否か(接続されていたらtrue)
+// electorifyToCoil(boolean)でコイルに電流を流す。電流流したらswitchStateがtrueに。
+// ソフトウェア的に見ると無駄の無駄ですが、一応リレー意識してるソレってことで勘弁
 public class Relay{
 	
 	boolean solenoidState;
